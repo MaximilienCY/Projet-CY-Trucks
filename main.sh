80835f2cf13e4676c3761697c39de00a7a8c9c98
MaximilienCY <maximilienhampe@gmail.com>
2024-01-02 11:44:09 +0100

    Verification des paramètres, ajout du paramètre -h, correction de bug

--- main.sh
+++ main.sh
@@ -14,8 +14,10 @@
 echo "
 Options : 
 	- chemin du fichier data.csv : premier paramètre obligatoire 
-	- -h : paramètre d'aide (tout les autres arguments seront ignorés)
-	 
+	- deuxième paramètre obligatoire, voici la liste des paramètres possibles : 
+	
+		 -h : paramètre d'aide (tout les autres arguments seront ignorés)
+	
 	 
 	 
 "
@@ -23,11 +25,14 @@ Options :
 
 #si le premier paramètre est bien un fichier
 if [ ! -f $1 ];then
-	echo "Le fichier spécifié n'existe pas ou n'en est pas un"
-
+	echo "ERREUR : Le fichier spécifié n'existe pas ou n'en est pas un"
+	exit 1
+fi
+	
 #si le premier paramètre est un bien un fichier CSV	
-elif [ "$1" != "*.csv" ];then
+if [ "$1" != *".csv" ];then
 	echo "ERREUR : le fichier spécifié doit être un fichier CSV "
+	exit 1
 	
 fi
 
@@ -36,22 +41,41 @@ data=$1
 #ignore le premier paremètre
 shift
 
-#fonction pour pour identifier l'argument 
-recherche() {
-	case $1 in
-		'-h') return 0;
-		'-d1') return 1;
-		'-d2') return 2;
-		'-l') return 3;
-		'-t') return 4;
-		'-s') return 5;
+#verification de l'existance d'un deuxième paramètre
+if [ "$1" == "" ];then
+		echo "ERREUR : Il est obligatoire de passé un deuxième paramètre"
+		exit 1
+fi
 
+
+#verification executable C
+
+#verification de la présence du dossier temp et images 
+
+
+
+
+#fonction pour pour identifier l'argument 
+verification_parametre() {
+	case "$1" in
+		'-d1'|'-d2'|'-l'|'-t'|'-s') return 0;;
+		
+		*) return 1;;
+	esac
 }
 
-#Regarde tout les paramètres restant
+#fonction lancement des traitements
+
+#Parcours tout les paramètres 
 for par in $*;do
-	echo $par
+	if [ $par == "-h" ];then
+		echo "Vous venez de selectionner le paramètre d'aide"
+		exit 1
+	fi
+	#verification de la validée du parametre par la fonction verification_parametre
+	if  ! verification_parametre "$par" ;then
+		echo "ERREUR : un des paramètres n'existe pas"
+		exit 1
+	fi
+	
 done
-
-
-
